520b4dbd11d92228dad72528357b1e728ee0e21b
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-07-01 13:11:27 +0600

    complete the projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,4 @@
+import AnimatedButton from "@/ui/AnimatedButton";
 import { ArrowUpRight } from "lucide-react";
 import { FaGithub } from "react-icons/fa";
 
@@ -131,6 +132,14 @@ const Projects = () => {
               </div>
             </div>
           ))}
+        </div>
+
+        {/* View All CTA */}
+        <div className="text-center mt-12 animate-fade-in animation-delay-500">
+          <AnimatedButton>
+            View All Projects
+            <ArrowUpRight className="w-5 h-5"></ArrowUpRight>
+          </AnimatedButton>
         </div>
       </div>
     </section>
